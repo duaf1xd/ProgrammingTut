31e118423305fe80ad45c20b8aab1f0012c4519e
duaf1xd <44461213+duaf1xd@users.noreply.github.com>
2018-10-26 10:21:34 +0700

    Lec7- Example1
    Công thức: (x+ n/x)/2 đến khi prevx=x
    
    prevx là x của lần chạy trước lưu tạm vào.

---
+++ JavaApplication13.java
@@ -0,0 +1,18 @@
+package javaapplication13;
+import java.util.Scanner;
+public class JavaApplication13 {
+    public static void main(String[] args) {
+        Scanner cyka= new Scanner(System.in);
+        System.out.println("Enter a number");
+        double input= cyka.nextDouble();
+        double x=1, prevx, res;
+        do
+        {
+            prevx= x;
+            x= (x+ (input/x))/2;
+            res= x-prevx;
+        }
+        while (x-prevx!=0);
+        System.out.println("Square root of "+ input + " is " + x);
+    }
+}
